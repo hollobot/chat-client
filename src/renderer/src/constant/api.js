4a9c7f09d43488b57dbb2692d34be569c812256b
taoxiao <taoxiao@zhilehuo.onaliyun.com>
2025-09-22 13:30:00 +0800

    优化

--- src/renderer/src/constant/api.js
+++ src/renderer/src/constant/api.js
@@ -1,9 +1,13 @@
 export const api = {
 	prot: "6742",
-	prodDomain: " https://myhttp.loca.lt",
+	// prodDomain: " https://myhttp.loca.lt",
+	prodDomain: " http://106.53.223.225:7777",
+	// devDomain: " http://106.53.223.225:7777",
 	devDomain: "http://127.0.0.1:7777",
 	// devDomain: "https://myhttp.loca.lt/",
-	prodWsDomain: "https://48227af0bdd7.ngrok-free.app/ws",
+	// prodWsDomain: "https://48227af0bdd7.ngrok-free.app/ws",
+	prodWsDomain: "http://106.53.223.225:18023/ws",
+	// devWsDomain: "http://106.53.223.225:18023/ws",
 	devWsDomain: "ws:/127.0.0.1:18023/ws"
 	// devWsDomain: "https://abff8dab741d.ngrok-free.app/ws"
 };
